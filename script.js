a9a826044efe1bd3fa761c8221aa1e295eaf7cca
dina.le <dina.le@accenture.com>
2026-09-16 15:35:19 +0200

    variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hello world"); 
+let zahl = 5;
+
+console.log(typeof zahl);
+let nameOfme = "Dina";
+let ageOfme = 22;
+let student = true;
+console.log(nameOfme + " is " + ageOfme + " years old. It is "+ student + " that she is a student." );
+
